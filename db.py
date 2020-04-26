ba94eefef6ada3436426521d0e7af6384b9de186
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-04-26 23:09:51 +0800

    add username and password use command line

--- db.py
+++ db.py
@@ -2,13 +2,31 @@
 from pymongo import MongoClient
 
 import json
+import sys
 
 # get config
 with open('config.json') as f:
     config = json.load(f)
 
-# connect to mongodb cluster
-conn = MongoClient(config['MongoDB_Connect_String'])
+# Compose the real connection string with command line arguments
+try:
+    connection_string = config['MongoDB_Connect_String']
+    connection_string = connection_string.replace("<username>", sys.argv[1])
+    connection_string = connection_string.replace("<password>", sys.argv[2])
+except Exception as e:
+    print("Compose the connection string failed.")
+    print("Error message: {}").format(e)
+    print("Maybe you forgot inputing the username and password?")
+    exit()
+
+# Connect to database
+try:
+    conn = MongoClient(connection_string)
+    conn.server_info()
+except Exception as e:
+    print("Connect to database failed.")
+    print("Error message: {}".format(e))
+    exit()
 
 # database
 db = conn.data
