1bce20485140c0cfb46c3569ca2c4ee871fc3a55
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-04-26 23:51:51 +0800

    setup circleCI

--- db.py
+++ db.py
@@ -5,26 +5,15 @@ import json
 import sys
 
 # get config
-with open('config.json') as f:
+with open('apiKey.json') as f:
     config = json.load(f)
-
-# Compose the real connection string with command line arguments
-try:
-    connection_string = config['MongoDB_Connect_String']
-    connection_string = connection_string.replace("<username>", sys.argv[1])
-    connection_string = connection_string.replace("<password>", sys.argv[2])
-except Exception as e:
-    print("Compose the connection string failed.")
-    print("Error message: {}").format(e)
-    print("Maybe you forgot inputing the username and password?")
-    exit()
 
 # Connect to database
 try:
-    conn = MongoClient(connection_string)
+    conn = MongoClient(config['MongoDB_Connect_String'])
     conn.server_info()
 except Exception as e:
-    print("Connect to database failed.")
+    print("Connecting to database failed.")
     print("Error message: {}".format(e))
     exit()
 
--- readme.md
+++ readme.md
@@ -1,9 +1,3 @@
 # Reco Backend
 
 This project is the backend server of Project Reco, a open source calendar system made by Yuanlin Lin.
-
-## Command
-
-### `python main.py <username> <password>`
-
-Start the development server. The command line args is needed to connect the database.
